680db269815e2bbcea2bbe8e998e95a05ee48703
jtjudge <jtjudge@iastate.edu>
2018-02-15 15:05:35 -0600

    Added demo sphere to scene.

--- app/scripts/basic3d.js
+++ app/scripts/basic3d.js
@@ -37,4 +37,56 @@ function main() {
 	// Attach the renderer-supplied
 	// DOM element.
 	container.appendChild(renderer.domElement);
+	
+	// Set up the sphere vars
+	const RADIUS = 50;
+	const SEGMENTS = 16;
+	const RINGS = 16;
+	
+	// create the sphere's material
+	const sphereMaterial = new THREE.MeshLambertMaterial({
+			color: 0xCC0000
+    });
+	
+	// Create a new mesh with
+	// sphere geometry - we will cover
+	// the sphereMaterial next!
+	const sphere = new THREE.Mesh(
+
+	  new THREE.SphereGeometry(
+		RADIUS,
+		SEGMENTS,
+		RINGS),
+
+	  sphereMaterial);
+
+	// Move the Sphere back in Z so we
+	// can see it.
+	sphere.position.z = -300;
+
+	// Finally, add the sphere to the scene.
+	scene.add(sphere);
+	
+	// create a point light
+	const pointLight =
+	  new THREE.PointLight(0xFFFFFF);
+
+	// set its position
+	pointLight.position.x = 10;
+	pointLight.position.y = 50;
+	pointLight.position.z = 130;
+
+	// add to the scene
+	scene.add(pointLight);
+	
+	function update () {
+	  // Draw!
+	  renderer.render(scene, camera);
+
+	  // Schedule the next frame.
+	  requestAnimationFrame(update);
+	}
+
+	// Schedule the first frame.
+	requestAnimationFrame(update);
 }
