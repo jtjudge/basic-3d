34982065ce66980191d751e31763158baf420f3e
Benjamin Vogel <bavogel@iastate.edu>
2018-02-21 10:44:56 -0600

    Created EdgePlacement
    Started the file and the initialization

---
+++ app/scripts/EdgePlacement.js
@@ -0,0 +1,35 @@
+
+var EdgePlacement =  (function() {
+
+  var initialized = false;
+
+  //Setting the selct color of the edge to be yellow
+  var SELECT_COLOR =  0xffd700;
+
+  //Setting the deselect color of the edge to be white like the vertices
+  var DESELECT_COLOR = 0xffffff;
+
+  //I'm assuming we'll need to add more variables up here for keeping track of edges
+
+};
+
+  //Checking to see if the module is initialized before using it
+  function assertInit(val){
+    if(initialized && !val)
+    {
+      Debug.log("[EdgePlacement] ERROR: Module already initialized");
+      return false;
+    }
+    else if(!initialized && val)
+    {
+      Debug.log("[EdgePlacement] ERROR: Module not initialized");
+      return false;
+    }
+
+    return true;
+  }
+
+  var interface = {
+    init: function()
+  }
+)
