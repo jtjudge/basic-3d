9d67d188fe96e56ba6f2c028616eac79119e69c7
Thomas Judge <jtjudge@iastate.edu>
2018-02-22 19:55:09 -0600

    Expanded key bindings interface for menu implementation.

--- app/scripts/KeyBindings.js
+++ app/scripts/KeyBindings.js
@@ -6,21 +6,48 @@ var KeyBindings = (function() {
     actions: {}
   };
 
-  function map(key, action) {
-    if(!bindings.keys[key]) {
-      bindings.keys[key] = [];
-    }
-    if(!bindings.actions[action]) {
-      bindings.actions[action] = false;
-    }
-    bindings.keys[key].push(action);
-  }
-
   var interface = {
     getKeyBindings: function() {
       return bindings;
     },
-    addKeyBinding: map
+    addKeyBinding: function(key, action) {
+      if(!bindings.keys[key]) {
+        bindings.keys[key] = [];
+      }
+      if(!bindings.actions[action]) {
+        bindings.actions[action] = false;
+      }
+      var index = bindings.keys[key].findIndex(function(a) {
+        return a === action;
+      });
+      if(index > -1) {
+        Debug.log("[KeyBindings] Duplicate key binding '" + 
+          key + " --> " + action + "' attempted");
+        return false;
+      }
+      bindings.keys[key].push(action);
+      return true;
+    },
+    removeKeyBinding: function(key, action) {
+      if(!bindings.keys[key] === undefined) {
+        Debug.log("[KeyBindings] Key '" + key + "' not registered");
+        return false;
+      }
+      if(bindings.actions[action] === undefined) {
+        Debug.log("[KeyBindings] Action '" + action + "' not registered");
+        return false;
+      }
+      var index = bindings.keys[key].findIndex(function(a) {
+        return a === action;
+      });
+      if(index === -1) {
+        Debug.log("[KeyBindings] Key binding '" + 
+          key + " --> " + action + "' not registered");
+        return false;
+      }
+      bindings.keys[key].splice(index, 1);
+      return true;
+    }
   };
 
   return interface;
