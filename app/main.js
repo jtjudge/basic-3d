2fd8d20e7ac526423e7df845cbf7b56907d1b192
Benjamin Vogel <bavogel@iastate.edu>
2018-02-28 21:08:36 -0600

    Added ipcMain to Main.js

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 const Menu = electron.Menu;
 // Set environment
 process.env.NODE_ENV = "development";
@@ -181,74 +181,116 @@ function changeWindowColor(color){
     });
 
     //InputHandling not working
-    var bindings = InputHandling.getKeyBindings();
+    var bindings;
     var index = 0;
     var found = false;
 
-    while(!found && index < bindings.actions.length)
+    ipcMain.send('get_bindings');
+
+    ipcMain.on('get_bindings_recieved', (event, arg) => {
+      bindings = arg;
+    });
+
+    while(index < bindings.actions.length)
     {
       if(bindings.actions[index] === action){
-        found = true;
+        ipcMain.send('remove_key_binding', index);
+        //InputHandling.removeKeyBinding(action, bindings.keys[index]);
       }
       else{
         index++;
       }
     }
 
-    InputHandling.removeKeyBinding(action, bindings.keys[index]);
-
-    InputHandling.register({
-      onmousedown: function(input) {
-        InputHandling.addKeyBinding(action, input);
-        child.close();
-      },
-
-      onkeydown: function(input) {
-        InputHandling.addKeyBinding(action, input);
+      function mousedown(event) {
+        event.preventDefault();
+        var code = mouseEvents[event.which];
+        ipcMain.send('add_key_binding', (action, code));
+        //InputHandling.addKeyBinding(action, input);
         child.close();
       }
-    });
+
+      function keydown(event) {
+        ipcMain.send('add_key_binding', (action, event.code));
+        //InputHandling.addKeyBinding(action, input);
+        child.close();
+      }
 }
 
   function resetKeyBinds(){
-      while(InputHandling.bindings.keys.length != 0)
+    var binding;
+
+    ipcMain.send('get_bindings');
+
+    ipcMain.on('get_bindings_recieved', (event, arg) => {
+      bindings = arg;
+    });
+
+      while(bindings.keys.length != 0)
       {
-        InputHandling.removeKeyBinding(InputHandling.bindings.keys[0], InputHandling.bindings.actions[0]);
+        ipcMain.send('remove_key_binding', 0);
+        //InputHandling.removeKeyBinding(InputHandling.bindings.keys[0], InputHandling.bindings.actions[0]);
       }
 
-      InputHandling.addKeyBinding("KeyW", "CAM_DOLLY_IN");
-      InputHandling.addKeyBinding("KeyS", "CAM_DOLLY_OUT");
-      InputHandling.addKeyBinding("KeyA", "CAM_TRUCK_LEFT");
-      InputHandling.addKeyBinding("KeyD", "CAM_TRUCK_RIGHT");
-      InputHandling.addKeyBinding("KeyQ", "CAM_PEDESTAL_UP");
-      InputHandling.addKeyBinding("KeyZ", "CAM_PEDESTAL_DOWN");
+      ipcMain.send('add_key_binding', ("KeyW", "CAM_DOLLY_IN"));
+      //InputHandling.addKeyBinding("KeyW", "CAM_DOLLY_IN");
+      ipcMain.send('add_key_binding', ("KeyS", "CAM_DOLLY_OUT"));
+      //InputHandling.addKeyBinding("KeyS", "CAM_DOLLY_OUT");
+      ipcMain.send('add_key_binding', ("KeyA", "CAM_TRUCK_LEFT"));
+      //InputHandling.addKeyBinding("KeyA", "CAM_TRUCK_LEFT");
+      ipcMain.send('add_key_binding', ("KeyD", "CAM_TRUCK_RIGHT"));
+      //InputHandling.addKeyBinding("KeyD", "CAM_TRUCK_RIGHT");
+      ipcMain.send('add_key_binding', ("KeyQ", "CAM_PEDESTAL_UP"));
+      //InputHandling.addKeyBinding("KeyQ", "CAM_PEDESTAL_UP");
+      ipcMain.send('add_key_binding', ("KeyZ", "CAM_PEDESTAL_DOWN"));
+      //InputHandling.addKeyBinding("KeyZ", "CAM_PEDESTAL_DOWN");
 
-      InputHandling.addKeyBinding("KeyO", "CAM_RESET");
+      ipcMain.send('add_key_binding', ("KeyO", "CAM_RESET"));
+      //InputHandling.addKeyBinding("KeyO", "CAM_RESET");
 
-      InputHandling.addKeyBinding("LMB", "CAM_TILT_FREE");
-      InputHandling.addKeyBinding("LMB", "CAM_ORBIT_MOD");
+      ipcMain.send('add_key_binding', ("LMB", "CAM_TILT_FREE"));
+      //InputHandling.addKeyBinding("LMB", "CAM_TILT_FREE");
+      ipcMain.send('add_key_binding', ("LMB", "CAM_ORBIT_MOD"));
+      //InputHandling.addKeyBinding("LMB", "CAM_ORBIT_MOD");
 
-      InputHandling.addKeyBinding("MMB", "CAM_TILT_FREE");
-      InputHandling.addKeyBinding("MMB", "CAM_ORBIT_MOD");
+      ipcMain.send('add_key_binding', ("MMB", "CAM_TILT_FREE"));
+      //InputHandling.addKeyBinding("MMB", "CAM_TILT_FREE");
+      ipcMain.send('add_key_binding', ("MMB", "CAM_ORBIT_MOD"));
+      //InputHandling.addKeyBinding("MMB", "CAM_ORBIT_MOD");
 
-      InputHandling.addKeyBinding("RMB", "CAM_TILT_FREE");
+      ipcMain.send('add_key_binding', ("RMB", "CAM_TILT_FREE"));
+      //InputHandling.addKeyBinding("RMB", "CAM_TILT_FREE");
 
-      InputHandling.addKeyBinding("ShiftLeft", "CAM_SPEED_MOD");
-      InputHandling.addKeyBinding("ShiftRight", "CAM_SPEED_MOD");
+      ipcMain.send('add_key_binding', ("ShiftLeft", "CAM_SPEED_MOD"));
+      //InputHandling.addKeyBinding("ShiftLeft", "CAM_SPEED_MOD");
+      ipcMain.send('add_key_binding', ("ShiftRight", "CAM_SPEED_MOD"));
+      //InputHandling.addKeyBinding("ShiftRight", "CAM_SPEED_MOD");
 
-      InputHandling.addKeyBinding("KeyI", "CAM_TILT_UP");
-      InputHandling.addKeyBinding("KeyK", "CAM_TILT_DOWN");
-      InputHandling.addKeyBinding("KeyJ", "CAM_TILT_LEFT");
-      InputHandling.addKeyBinding("KeyL", "CAM_TILT_RIGHT");
+      ipcMain.send('add_key_binding', ("KeyI", "CAM_TILT_UP"));
+      //InputHandling.addKeyBinding("KeyI", "CAM_TILT_UP");
+      ipcMain.send('add_key_binding', ("KeyK", "CAM_TILT_DOWN"));
+      //InputHandling.addKeyBinding("KeyK", "CAM_TILT_DOWN");
+      ipcMain.send('add_key_binding', ("KeyJ", "CAM_TILT_LEFT"));
+      //InputHandling.addKeyBinding("KeyJ", "CAM_TILT_LEFT");
+      ipcMain.send('add_key_binding', ("KeyL", "CAM_TILT_RIGHT"));
+      //InputHandling.addKeyBinding("KeyL", "CAM_TILT_RIGHT");
 
-      InputHandling.addKeyBinding("Space", "CAM_ORBIT_MOD");
+      ipcMain.send('add_key_binding', ("Space", "CAM_ORBIT_MOD"));
+      //InputHandling.addKeyBinding("Space", "CAM_ORBIT_MOD");
 
-      InputHandling.addKeyBinding("KeyV", "TOGGLE_VERTEX_MODE");
-      InputHandling.addKeyBinding("LMB", "PLACE_VERTEX");
-      InputHandling.addKeyBinding("KeyE", "PLACE_EDGE");
-      InputHandling.addKeyBinding("KeyF", "PLACE_FACE");
+      ipcMain.send('add_key_binding', ("KeyV", "TOGGLE_VERTEX_MODE"));
+      //InputHandling.addKeyBinding("KeyV", "TOGGLE_VERTEX_MODE");
+      ipcMain.send('add_key_binding', ("LMB", "PLACE_VERTEX"));
+      //InputHandling.addKeyBinding("LMB", "PLACE_VERTEX");
+      ipcMain.send('add_key_binding', ("KeyE", "PLACE_EDGE"));
+      //InputHandling.addKeyBinding("KeyE", "PLACE_EDGE");
+      ipcMain.send('add_key_binding', ("KeyF", "PLACE_FACE"));
+      //InputHandling.addKeyBinding("KeyF", "PLACE_FACE");
 
-      InputHandling.addKeyBinding("LMB", "SELECT_GEOM");
-      InputHandling.addKeyBinding("ShiftLeft", "MULT_SELECT_MOD");
-      InputHandling.addKeyBinding("ShiftRight", "MULT_SELECT_MOD");
+      ipcMain.send('add_key_binding', ("LMB", "SELECT_GEOM"));
+      //InputHandling.addKeyBinding("LMB", "SELECT_GEOM");
+      ipcMain.send('add_key_binding', ("ShiftLeft", "MULT_SELECT_MOD"));
+      //InputHandling.addKeyBinding("ShiftLeft", "MULT_SELECT_MOD");
+      ipcMain.send('add_key_binding', ("ShiftRight", "MULT_SELECT_MOD"));
+      //InputHandling.addKeyBinding("ShiftRight", "MULT_SELECT_MOD");
 }
