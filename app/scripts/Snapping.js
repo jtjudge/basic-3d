307b6bf262f2c174b2695248bb0404e3fa7a2eb6
Benjamin Vogel <bavogel@iastate.edu>
2018-03-22 13:02:56 -0500

    Tentative snap function

--- app/scripts/Snapping.js
+++ app/scripts/Snapping.js
@@ -1,4 +1,33 @@
 
 Basic3D.loadModule("Snapping", function(Input, Scene, Geometry, Selection) {
-  
+
+  var tolerance = 1;
+
+  function snap(vSource, vSnap) {
+    if(vSource.x <== vSnap.x + tolerance && vSource.x >== vSnap.x - tolerance) {
+      if(vSource.y <== vSnap.y + tolerance && vSource.y >== vSnap.y - tolerance) {
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(vSource);
+        geometry.vertices.push(vSnap);
+        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial( {color: 0x0000ff} ));
+        scene.add(line);
+      }
+      else if(vSource.z <== vSnap.z + tolerance && vSource.y >== vSnap.y - tolerance) {
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(vSource);
+        geometry.vertices.push(vSnap);
+        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial( {color: 0x0000ff} ));
+        scene.add(line);
+      }
+    }
+    else if(vSource.y <== vSnap.y + tolerance && vSource.y >== vSnap.y - tolerance) {
+      if(vSource.z <== vSnap.z + tolerance && vSource.y >== vSnap.y - tolerance) {
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(vSource);
+        geometry.vertices.push(vSnap);
+        var line = new THREE.Line(geometry, new THREE.LineBasicMaterial( {color: 0x0000ff} ));
+        scene.add(line);
+      }
+    }
+  }
 })
