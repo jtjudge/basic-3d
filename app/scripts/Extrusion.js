6e54bccc399d4559605b55c4fb58b086eb042130
Thomas Judge <jtjudge@iastate.edu>
2018-03-23 01:54:44 -0500

    Fixed issues in extrusion.

--- app/scripts/Extrusion.js
+++ app/scripts/Extrusion.js
@@ -1,106 +1,91 @@
 
-Basic3D.loadModule("Extrusion", function (Input, Scene, Controls, Geometry, Selection, History, TipsDisplay) {
+Basic3D.loadModule("Extrusion", function (Input, Scene, Controls, Geometry) {
 
-  var moveStarted = false;
-  var move, newGeo;
+  var newGeo = null;
 
-  function extrudeFace(face) {
+  function confirmMove() {
+    newGeo.vertices.forEach(Geometry.addVertex);
+    newGeo.edges.forEach(Geometry.addEdge);
+    newGeo.faces.forEach(Geometry.addFace);
+  }
+
+  function startMove(face) {
     var fv = [face.v1, face.v2, face.v3];
-    var v = fv.map(function(vtx) {
+    var v = fv.map(function (vtx) {
       return Geometry.Vertex(vtx.obj.position);
     });
     var e = [];
     var f = [Geometry.Face(v[0], v[1], v[2])];
-    for(i = 0; i < 3; i++) {
-      e.push(Geometry.Edge(v[i], v[(i+1) % 3]));
-      e.push(Geometry.Edge(v[i], fv[(i+1) % 3]));
+    for (i = 0; i < 3; i++) {
+      e.push(Geometry.Edge(v[i], v[(i + 1) % 3]));
+      e.push(Geometry.Edge(v[i], fv[(i + 1) % 3]));
       e.push(Geometry.Edge(v[i], fv[i]));
-      f.push(Geometry.Face(v[i], v[(i+1) % 3], fv[(i+1) % 3]));
-      f.push(Geometry.Face(v[i], fv[i], fv[(i+1) % 3]));
+      f.push(Geometry.Face(v[i], v[(i + 1) % 3], fv[(i + 1) % 3]));
+      f.push(Geometry.Face(v[i], fv[i], fv[(i + 1) % 3]));
     }
-    return {
-      v : v,
-      e : e,
-      f : f
+    newGeo = {
+      vertices: v,
+      edges: e,
+      faces: f
     };
-  }
-
-  function addGeometry(geometry) {
-    geometry.v.forEach(function (vert) {
-      Geometry.addVertex(vert);
+    newGeo.vertices.forEach(function (vert) {
+      Scene.add(vert.obj);
     });
-    geometry.e.forEach(function (edge) {
-      Geometry.addEdge(edge);
+    newGeo.edges.forEach(function (edge) {
+      Scene.add(edge.obj);
     });
-    geometry.f.forEach(function (face) {
-      Geometry.addFace(face);
+    newGeo.faces.forEach(function (face) {
+      Scene.add(face.obj);
     });
   }
 
-  function removeGeometry(geometry) {
-    geometry.v.forEach(function (vert) {
-      Geometry.removeVertex(vert);
-    });
-    geometry.e.forEach(function (edge) {
-      Geometry.removeEdge(edge);
-    });
-    geometry.f.forEach(function (face) {
-      Geometry.removeFace(face);
-    });
-  }
-
-  function startMove(face) {
-    newGeo = extrudeFace(face[0]);
-    addGeometry(newGeo);
-  }
-
-  function translateVertex(v, diff) {
-    v.obj.position.add(diff);
-    v.edges.forEach(function (e) {
+  function translateFace(face, diff) {
+    var vect = face.obj.geometry.faces[0].normal;
+    face.v1.obj.translateOnAxis(vect, diff);
+    face.v2.obj.translateOnAxis(vect, diff);
+    face.v3.obj.translateOnAxis(vect, diff);
+    newGeo.edges.forEach(function (e) {
       e.obj.geometry.verticesNeedUpdate = true;
     });
-    v.faces.forEach(function (f) {
+    newGeo.faces.forEach(function (f) {
       f.obj.geometry.verticesNeedUpdate = true;
     });
   }
 
   Input.register({
     onmousedown: function () {
-      if(Input.mode("Extrude") && moveStarted){
+      if (Input.mode("EXTRUDE") && Input.action("CONFIRM_EXTRUDE")) {
+        confirmMove();
         Controls.enable(["orbit"]);
         Input.setMode("EDIT");
-        moveStarted = false;
       }
     },
-    onkeydown: function() {
-      if(Input.action("TOGGLE_EXTRUSION")) {
-        if(Input.mode("EDIT") && !moveStarted) {
-          if(Geometry.getSelectedFaces().length === 1) {
-            Controls.disable(["orbit"]);
-            Input.setMode("Extrude");
-            moveStarted = true;
-            startMove(Geometry.getSelectedFaces());
-          }
-        } else if(Input.mode("Extrude") && moveStarted) {
-          Controls.enable(["orbit"]);
-          removeGeometry(newGeo);
-          Input.setMode("EDIT");
-          moveStarted = false;
+    onkeydown: function () {
+      if (!Input.action("TOGGLE_EXTRUDE_MODE")) return;
+      if (Input.mode("EDIT")) {
+        if (Geometry.getSelectedFaces().length === 1) {
+          Input.setMode("EXTRUDE");
+          Controls.disable(["orbit"]);
+          startMove(Geometry.getSelectedFaces()[0]);
         }
+      } else if (Input.mode("EXTRUDE")) {
+        newGeo = null;
+        Controls.enable(["orbit"]);
+        Input.setMode("EDIT");
       }
     },
-    onmousemove: function() {
-      if(Input.mode("Extrude")) {
-        var dx = Scene.getMovementOnXZ().diff.x;
+    onmousemove: function () {
+      if (Input.mode("EXTRUDE")) {
         var dy = Scene.getMovementOnY();
-        var dz = Scene.getMovementOnXZ().diff.z;
-        translateVertex(newGeo.v[0], new THREE.Vector3(dx, dy, dz));
-        translateVertex(newGeo.v[1], new THREE.Vector3(dx, dy, dz));
-        translateVertex(newGeo.v[2], new THREE.Vector3(dx, dy, dz));
+        // To do: get movement on axis
+        translateFace(newGeo.faces[0], dy);
       }
     }
   });
-  Input.addKeyBinding("KeyY", "TOGGLE_EXTRUSION", "Toggle Extrusion");
+
+  Input.addKeyBinding("KeyY", "TOGGLE_EXTRUDE_MODE", "Toggle Extrude");
+  Input.addKeyBinding("LMB", "CONFIRM_EXTRUDE");
 
   return {};
+
 });
